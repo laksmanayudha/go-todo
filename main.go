868ab22948b5024cc92800eb44c3b710aa1d72fe
laksmanayudha <laksmanayudha22@gmail.com>
2026-01-25 16:47:56 +0800

    mvp features done

--- main.go
+++ main.go
@@ -29,13 +29,80 @@ func TransformStatus(status bool) string {
 }
 
 func ShowTodos(todos TodoList) {
+	if len(todos) <= 0 {
+		fmt.Println("No todo available")
+		return
+	}
+
 	for index, todo := range todos {
-		var num int = index + 1
 		var title string = todo.Title
 		var status string = TransformStatus(todo.Status)
 
-		fmt.Printf("%d. title: %v | status: %v\n", num, title, status)
+		fmt.Printf("ID: %d | title: %v | status: %v\n", index, title, status)
 	}
+}
+
+func MarkDoneTodoById(todos TodoList, id int) (TodoList, error) {
+	todo, err := FindTodoById(todos, id)
+	if err != nil {
+		return todos, err
+	}
+
+	todo.Status = true
+	updatedTodos, err := UpdateTodoById(todos, todo, id)
+
+	return updatedTodos, nil
+}
+
+func UpdateTodoById(todos TodoList, newTodo Todo, id int) (TodoList, error) {
+	if err := ValidateId(todos, id); err != nil {
+		return todos, err
+	}
+
+	for index := range todos {
+		if index == id {
+			todos[index] = newTodo
+			break
+		}
+	}
+
+	return todos, nil
+}
+
+func DeleteTodoById(todos TodoList, id int) (TodoList, error) {
+	if err := ValidateId(todos, id); err != nil {
+		return todos, err
+	}
+
+	var newTodos TodoList = append(todos[:id], todos[id+1:]...)
+
+	return newTodos, nil
+}
+
+func FindTodoById(todos TodoList, id int) (t Todo, err error) {
+	if err := ValidateId(todos, id); err != nil {
+		return t, err
+	}
+
+	for index, todo := range todos {
+		if index == id {
+			return todo, nil
+		}
+	}
+
+	return t, errors.New("Todo not found")
+}
+
+func ValidateId(todos TodoList, id int) error {
+	if id < 0 {
+		return errors.New("ID must at least 0")
+	}
+
+	if id >= len(todos) {
+		return errors.New("ID not found")
+	}
+
+	return nil
 }
 
 func main() {
@@ -55,7 +122,6 @@ func main() {
 	var command string = arguments[1]
 	switch command {
 	case "add":
-		// create add subcommand
 		var AddCommand *flag.FlagSet = flag.NewFlagSet("add", flag.ExitOnError)
 		var title *string = AddCommand.String("title", "", "add a todo title")
 
@@ -69,6 +135,32 @@ func main() {
 
 		ShowTodos(todoList)
 	case "list":
+		ShowTodos(todoList)
+	case "done":
+		var DoneCommand *flag.FlagSet = flag.NewFlagSet("done", flag.ExitOnError)
+		var todoId *int = DoneCommand.Int("id", 0, "todo id")
+
+		DoneCommand.Parse(arguments[2:])
+
+		todoList, err := MarkDoneTodoById(todoList, *todoId)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		ShowTodos(todoList)
+	case "delete":
+		var DeleteCommand *flag.FlagSet = flag.NewFlagSet("delete", flag.ExitOnError)
+		var todoId *int = DeleteCommand.Int("id", 0, "todo id")
+
+		DeleteCommand.Parse(arguments[2:])
+
+		todoList, err := DeleteTodoById(todoList, *todoId)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
 		ShowTodos(todoList)
 	default:
 		fmt.Println("Unknown command. Please provide a valid command. See available command using --help flag")
